9f0f51f91a3febe16a4bdab8afbcf228b35a1562
Rishi Jain <rishi.jain500@gmail.com>
2022-05-03 20:47:29 +0530

    getting started with server

--- gettingStarted.js
+++ gettingStarted.js
@@ -7,6 +7,11 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000
 
+/**
+ * The below callback is executed upon every request that comes in.
+ * When a new request is received, the 'request' event is called with two arguments
+ * the first being 'req' which is an http.IncomingMessage object and 'res' which is http.ServerResponse object
+ */
 const server = http.createServer((req, res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain')
